eed11fd506af7baf0cec3727520807c4e8839204
ratamon-tech <ratamon91@gmail.com>
2022-03-13 12:23:51 +0900

    DB作成のための設定記載

--- book_review_blog/settings.py
+++ book_review_blog/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }  
