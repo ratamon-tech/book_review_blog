26e343eb4b5aa3792039078082234b42177cc34a
ratamon-tech <ratamon91@gmail.com>
2022-03-13 11:41:03 +0900

    環境変数ファイル読み込み

--- book_review_blog/settings.py
+++ book_review_blog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sqvwc_x()z2nm^)0gtsmik9_pg(hj6o0_8wfju_)c!hbo@ge0c'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
